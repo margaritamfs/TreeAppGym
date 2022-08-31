bc980739d911848a15b3b350cb94600314931c56
frozen1000 <109002823+frozen1000@users.noreply.github.com>
2022-08-30 21:28:29 -0500

    CRUD PlanNutricional
    RepositorioPlanNutricional.cs
    IRepositorioPlanNutricional.cs

---
+++ TreeAppGym.App.Persistencia/AppRepositorios/IRepositorioPlanNutricional.cs
@@ -0,0 +1,25 @@
+using TreeAppGym.App.Dominio;
+// para que pueda leer donde está la entidad PlanNutricional
+using System.Collections.Generic;
+// este using es para que no me marque error con las listas numeradas
+// en este paquete se encuentra todo lo que tiene que ver con listas
+
+namespace TreeAppGym.App.Persistencia{
+
+    public interface IRepositorioPlanNutricional{
+
+        PlanNutricional CrearPlanNutricional(PlanNutricional PlanNutricional);
+
+        PlanNutricional ConsultarPlanNutricional(int idPlanNutricional);
+        // aqui quiero que me retorne todas las listas de planes nutricionales
+        IEnumerable<PlanNutricional> ConsultarPlanNutricional();
+        
+        PlanNutricional ActualizarPlanNutricional(PlanNutricional PlanNutricional);
+
+        void EliminarPlanNutricional(int idPlanNutricional);
+        // vacío porque no necesito que al eliminar me retorne nada
+    }
+
+
+}
+// En esta clase de tipo interfaz dejo la firma de los métodos, es como un contrato
---
+++ TreeAppGym.App.Persistencia/AppRepositorios/RepositorioPlanNutricional.cs
@@ -0,0 +1,66 @@
+using TreeAppGym.App.Dominio;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace TreeAppGym.App.Persistencia
+{
+
+    public class RepositorioPlanNutricional : IRepositorioPlanNutricional
+    {
+        private readonly AppContext _appContext;
+
+        public RepositorioPlanNutricional(AppContext appContext)
+        {
+            this._appContext = appContext;
+            // ojo este appContext es la conexión a la BD
+        }
+
+        public PlanNutricional CrearPlanNutricional(PlanNutricional PlanNutricional)
+        {
+            var PlanNutricionalAdicionado = _appContext.PlanNutricional.Add(PlanNutricional);
+            _appContext.SaveChanges();
+            // aqui guardo los cambios en la BD
+            return PlanNutricionalAdicionado.Entity;
+            // me lo envía como una entidad es decir como un objeto
+        }
+
+        public PlanNutricional ConsultarPlanNutricional(int idPlanNutricional)
+        {
+            return _appContext.PlanNutricional.FirstOrDefault(pn => pn.Id == idPlanNutricional);
+        
+        }
+
+        public IEnumerable<PlanNutricional> ConsultarPlanNutricional()
+        {
+            return _appContext.PlanNutricional;
+        }
+
+        public PlanNutricional ActualizarPlanNutricional(PlanNutricional PlanNutricional)
+        {
+
+            var PlanNutricionalEncontrado = _appContext.PlanNutricional.FirstOrDefault(pn => pn.Id == PlanNutricional.Id);
+            if (PlanNutricionalEncontrado != null)
+            {
+                PlanNutricionalEncontrado.Categoria = PlanNutricional.Categoria;
+                PlanNutricionalEncontrado.Descripcion = PlanNutricional.Descripcion;
+                PlanNutricionalEncontrado.Imagen = PlanNutricional.Imagen;
+                
+                _appContext.SaveChanges();
+            }
+            return PlanNutricionalEncontrado;
+
+        }
+
+        public void EliminarPlanNutricional(int idPlanNutricional)
+        {
+            var PlanNutricionalEncontrado = _appContext.PlanNutricional.FirstOrDefault(pn => pn.Id == idPlanNutricional);
+            if (PlanNutricionalEncontrado == null)
+            return;
+            _appContext.PlanNutricional.Remove(PlanNutricionalEncontrado);
+            _appContext.SaveChanges();
+        }
+
+    }
+
+}

e8591ccf4994612d39e0e1a2754dd24e89e0a02e
frozen1000 <109002823+frozen1000@users.noreply.github.com>
2022-08-31 08:22:30 -0500

    Update Program.cs
    Colocando el objeto IRepositorioPlanNutricional para ejecutar y probar CRUD en la BD

--- TreeAppGym.App.Consola/Program.cs
+++ TreeAppGym.App.Consola/Program.cs
@@ -1,13 +1,30 @@
-﻿using System;
+using System;
 using TreeAppGym.App.Dominio;
+using TreeAppGym.App.Persistencia;
 
 namespace TreeAppGym.App.Consola
 {
     class Program
     {
+        private static IRepositorioPlanNutricional _repoPlanNutricional =new RepositorioPlanNutricional(new Persistencia.AppContext());
+        //Coloco un objeto que me permite llamar a cada uno de los métodos CRUD
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World Entity Framework!");
+            AdicionarPlanNutricional();
         }
+        //Clase específica para crear el objeto PlanNutricional
+        private static void AdicionarPlanNutricional()
+        {
+            var planNutricional = new PlanNutricional
+            {
+                Categoria=Categoria.basica,
+                Descripcion="Alimentación saludable para principiantes",
+                Imagen="123456"
+                
+            };
+            _repoPlanNutricional.CrearPlanNutricional(planNutricional);
+        }
+
     }
 }

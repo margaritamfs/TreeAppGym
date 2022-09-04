04b5c7cb2aa983aea950b83746c6cfae009c8ee2
margaritamfs <margaritamfs.forero@gmail.com>
2022-09-04 15:19:32 -0500

    Operaciones CRUD para la clase Cliente desde Consola

--- TreeAppGym.App.Consola/Program.cs
+++ TreeAppGym.App.Consola/Program.cs
@@ -1,17 +1,27 @@
 using System;
 using TreeAppGym.App.Dominio;
 using TreeAppGym.App.Persistencia;
+using System.Collections.Generic;
 
 namespace TreeAppGym.App.Consola
 {
     class Program
     {
         private static IRepositorioPlanNutricional _repoPlanNutricional =new RepositorioPlanNutricional(new Persistencia.AppContext());
+        private static IRepositorioCliente _repoCliente = new RepositorioCliente(new Persistencia.AppContext());
         //Coloco un objeto que me permite llamar a cada uno de los métodos CRUD
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World Entity Framework!");
             AdicionarPlanNutricional();
+            //CRUD Clase Cliente
+            var cliente = crearCliente();
+            Console.WriteLine("Cliente Creado..");
+            ConsultarCliente(cliente.Id);
+            ConsultarClientes();
+            ActualizarCliente(cliente);
+            EliminarCliente(cliente.Id);
+
         }
         //Clase específica para crear el objeto PlanNutricional
         private static void AdicionarPlanNutricional()
@@ -24,6 +34,67 @@ namespace TreeAppGym.App.Consola
                 
             };
             _repoPlanNutricional.CrearPlanNutricional(planNutricional);
+        }
+        private static Cliente crearCliente()
+        {
+            var cliente = new Cliente
+            {
+                Nombres = "Luis",
+                Apellidos = "Soler",
+                Email = "luis.soler@test.com00",
+                Edad = 25,
+                Telefono = "3013013011",
+                Direccion = "Av Regional #12-34",
+                Contraseña = "a123&1",
+                Genero = Genero.masculino
+                
+            };
+            return _repoCliente.CrearCliente(cliente);
+        }
+        private static void ConsultarCliente(int idCliente)
+        {
+            var cliente = _repoCliente.ConsultarCliente(idCliente);
+             if (cliente == null)
+             {
+                Console.WriteLine("Cliente no encontrado..");
+             }else
+             {
+                Console.WriteLine("Cliente encontrado con id : "+cliente.Id +" - Nombre "+cliente.Nombres);
+             }
+        }
+        private static void ConsultarClientes()
+        {
+            IEnumerable<Cliente> clientes = _repoCliente.ConsultarClientes();
+            Console.WriteLine("Clientes encontrados..");
+            foreach (var cliente in clientes)
+            {
+                Console.WriteLine("Id Cliente : "+cliente.Id +" - Nombre "+cliente.Nombres);         
+            }
+
+        }
+        private static void ActualizarCliente(Cliente cliente)
+        {
+            Console.WriteLine("Cliente actual - ID: " + cliente.Id + " - Nombre: "+cliente.Nombres);
+            var clienteDatos = new Cliente
+            {
+                Id = cliente.Id,
+                Nombres = "Luisa",
+                Apellidos = "Soler",
+                Email = "luisa.soler@test.com00",
+                Edad = 26,
+                Telefono = "3023023021",
+                Direccion = "Av Regional #12-34",
+                Contraseña = "a123&1",
+                Genero = Genero.femenino
+                
+            };
+            Cliente clienteActualizado = _repoCliente.ActualizarCliente(clienteDatos);
+            Console.WriteLine("Cliente actualizado - ID: " + clienteActualizado.Id + " - Nombre: "+clienteActualizado.Nombres);
+        }
+        private static void EliminarCliente(int idCliente)
+        {
+            _repoCliente.EliminarCliente(idCliente);
+            Console.WriteLine("Cliente eliminado con id : "+idCliente);
         }
 
     }
---
+++ TreeAppGym.App.Persistencia/Migrations/20220904182500_entityclientes.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TreeAppGym.App.Persistencia.Migrations
+{
+    public partial class entityclientes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gym_Clientes_ClientesId",
+                table: "Gym");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gym_PlanNutricional_PlanNutricionalId",
+                table: "Gym");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gym_Rutina_RutinaId",
+                table: "Gym");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Rutina",
+                table: "Rutina");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Gym",
+                table: "Gym");
+
+            migrationBuilder.RenameTable(
+                name: "Rutina",
+                newName: "Rutinas");
+
+            migrationBuilder.RenameTable(
+                name: "Gym",
+                newName: "Gyms");
+
+            migrationBuilder.RenameColumn(
+                name: "ClientesId",
+                table: "Gyms",
+                newName: "ClienteId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Gym_RutinaId",
+                table: "Gyms",
+                newName: "IX_Gyms_RutinaId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Gym_PlanNutricionalId",
+                table: "Gyms",
+                newName: "IX_Gyms_PlanNutricionalId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Gym_ClientesId",
+                table: "Gyms",
+                newName: "IX_Gyms_ClienteId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Rutinas",
+                table: "Rutinas",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Gyms",
+                table: "Gyms",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gyms_Clientes_ClienteId",
+                table: "Gyms",
+                column: "ClienteId",
+                principalTable: "Clientes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gyms_PlanNutricional_PlanNutricionalId",
+                table: "Gyms",
+                column: "PlanNutricionalId",
+                principalTable: "PlanNutricional",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gyms_Rutinas_RutinaId",
+                table: "Gyms",
+                column: "RutinaId",
+                principalTable: "Rutinas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gyms_Clientes_ClienteId",
+                table: "Gyms");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gyms_PlanNutricional_PlanNutricionalId",
+                table: "Gyms");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gyms_Rutinas_RutinaId",
+                table: "Gyms");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Rutinas",
+                table: "Rutinas");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Gyms",
+                table: "Gyms");
+
+            migrationBuilder.RenameTable(
+                name: "Rutinas",
+                newName: "Rutina");
+
+            migrationBuilder.RenameTable(
+                name: "Gyms",
+                newName: "Gym");
+
+            migrationBuilder.RenameColumn(
+                name: "ClienteId",
+                table: "Gym",
+                newName: "ClientesId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Gyms_RutinaId",
+                table: "Gym",
+                newName: "IX_Gym_RutinaId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Gyms_PlanNutricionalId",
+                table: "Gym",
+                newName: "IX_Gym_PlanNutricionalId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Gyms_ClienteId",
+                table: "Gym",
+                newName: "IX_Gym_ClientesId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Rutina",
+                table: "Rutina",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Gym",
+                table: "Gym",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gym_Clientes_ClientesId",
+                table: "Gym",
+                column: "ClientesId",
+                principalTable: "Clientes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gym_PlanNutricional_PlanNutricionalId",
+                table: "Gym",
+                column: "PlanNutricionalId",
+                principalTable: "PlanNutricional",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gym_Rutina_RutinaId",
+                table: "Gym",
+                column: "RutinaId",
+                principalTable: "Rutina",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}

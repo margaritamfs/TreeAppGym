8da6921abb9cf6c8027c6764e854a03c7107e06b
margaritamfs <margaritamfs.forero@gmail.com>
2022-09-08 08:13:08 -0500

    Frontend consulta de clientes integrando backend y base de datos

--- TreeAppGym.App.Frontend/Pages/Clientes/List.cshtml.cs
+++ TreeAppGym.App.Frontend/Pages/Clientes/List.cshtml.cs
@@ -1,28 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 using TreeAppGym.App.Persistencia;
 using TreeAppGym.App.Dominio;
+
 
 namespace TreeAppGym.App.Frontend.Pages
 {
     public class ListModel : PageModel
     {
-       // public string[] clientes = {"luis","jose","juan","maria"};
 
-        //public List<string> ListaClientes {get;set;}
 
         private readonly IRepositorioCliente repositorioCliente;
         public IEnumerable<Cliente> Clientes {get;set;}
 
-        public ListModel(IRepositorioCliente repositorioCliente)
+        public ListModel()
         {
-            this.repositorioCliente = repositorioCliente;
+            this.repositorioCliente = new RepositorioCliente(new TreeAppGym.App.Persistencia.AppContext());
         }
 
         public void OnGet()
         {
-            //ListaClientes = new List<string>();
-            //ListaClientes.AddRange(clientes);
             Clientes = repositorioCliente.ConsultarClientes();
             
         }
--- TreeAppGym.App.Frontend/Pages/Privacy.cshtml.cs
+++ TreeAppGym.App.Frontend/Pages/Privacy.cshtml.cs
@@ -1,19 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace TreeAppGym.App.Frontend.Pages;
-
-public class PrivacyModel : PageModel
+namespace TreeAppGym.App.Frontend.Pages
 {
-    private readonly ILogger<PrivacyModel> _logger;
-
-    public PrivacyModel(ILogger<PrivacyModel> logger)
+    public class PrivacyModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly ILogger<PrivacyModel> _logger;
 
-    public void OnGet()
-    {
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
     }
 }
-
